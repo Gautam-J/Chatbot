bd1a8f132bab642daa9fe84c64b298813ee6c295
Gautam J <gautam.jayapal@gmail.com>
2020-09-01 16:14:54 +0530

    update train remove main

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 from data_configs import (
-    loadConversations,
-    getTfDataset,
+    getConversations,
+    preprocessSentence,
     fitTokenizerToCorpus,
     tokenizeData,
-    padTokenizedData
+    saveTokenizer,
+    padData,
+    getTfDataset
 )
 
 from model_configs import (
@@ -15,86 +17,94 @@ from model_configs import (
 )
 
 from plot_visualizations import (
-    plotSequenceLengthHistogram,
     plotTrainingHistory
 )
 
-import data_configs
+import settings as s
 from tensorflow.keras.optimizers import Adam
 
-MAXLEN = 40
-BATCH_SIZE = 64
-SHUFFLE_BUFFER_SIZE = 100000
 
-EPOCHS = 50
-NUM_LAYERS = 2
-D_MODEL = 256
-NUM_HEADS = 8
-UNITS = 512
-DROPOUT = 0.1
+def printQueryResponse(query, response, n=3):
+    for q, r in zip(query[:n], response[:n]):
+        print('Query:', q)
+        print('Response:', r)
+        print()
 
 
-def main():
-    # Load text data
-    print('[INFO] Loading text data')
-    queries, responses = loadConversations()
+# Load text data
+print('[INFO] Loading text data')
+queries, responses = getConversations(
+    'data/movie_lines.txt',
+    'data/movie_conversations.txt'
+)
 
-    # fit tokenizer on corpus
-    print('[INFO] Fitting tokenizer on corpus')
-    tokenizer = fitTokenizerToCorpus(queries + responses)
+printQueryResponse(queries, responses)
 
-    # tokenize data
-    print('[INFO] Tokenizing data')
-    queries = tokenizeData(queries, tokenizer)
-    responses = tokenizeData(responses, tokenizer)
+# preprocess sentence
+print('[INFO] Preprocessing data')
+queries = list(map(preprocessSentence, queries))
+responses = list(map(preprocessSentence, responses))
 
-    print('[INFO] Plotting KDE for sequence length')
-    plotSequenceLengthHistogram(queries + responses)
+printQueryResponse(queries, responses)
 
-    # pad sequences
-    print('[INFO] Padding sequences')
-    queries = padTokenizedData(queries, maxlen=MAXLEN)
-    responses = padTokenizedData(responses, maxlen=MAXLEN)
+# fit tokenizer on corpus
+print('[INFO] Fitting tokenizer on corpus')
+tokenizer = fitTokenizerToCorpus(queries + responses, vocab_size=s.VOCAB_SIZE)
 
-    # get tf.data.Dataset
-    print('[INFO] Fetching tf.data.Dataset')
-    dataset = getTfDataset(
-        queries, responses, batch_size=BATCH_SIZE,
-        shuffer_buffer_size=SHUFFLE_BUFFER_SIZE
-    )
+print('[INFO] Vocab Size:', len(tokenizer.word_index))
 
-    model = getTransformerModel(
-        vocab_size=data_configs.VOCAB_SIZE,
-        num_layers=NUM_LAYERS,
-        units=UNITS,
-        d_model=D_MODEL,
-        num_heads=NUM_HEADS,
-        dropout=DROPOUT
-    )
+# save tokenizer to disk
+saveTokenizer('models/myTokenizer.json', tokenizer)
 
-    model.summary()
+# tokenize data
+print('[INFO] Tokenizing data')
+queries = tokenizeData(queries, tokenizer)
+responses = tokenizeData(responses, tokenizer)
 
-    # configure optimizer
-    learningRate = CustomSchedule(D_MODEL, warmup_steps=4000)
-    optimizer = Adam(learningRate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+printQueryResponse(queries, responses)
 
-    # compile model with custom metrics
-    model.compile(
-        optimizer=optimizer,
-        loss=customLossFunction,
-        metrics=[accuracy]
-    )
+# pad sequences
+print('[INFO] Padding sequences')
+queries = padData(queries, maxlen=s.MAXLEN)
+responses = padData(responses, maxlen=s.MAXLEN)
 
-    # train model with callbacks
-    callbacks = getCallbacks()
-    model.fit(dataset, epochs=EPOCHS, callbacks=callbacks)
+printQueryResponse(queries, responses)
 
-    print('[INFO] Saving model weights')
-    model.save_weights('models/final_model_weight.hdf5')
+# get tf.data.Dataset
+print('[INFO] Fetching tf.data.Dataset')
+dataset = getTfDataset(
+    queries, responses, batch_size=s.BATCH_SIZE,
+    shuffer_buffer_size=s.SHUFFLE_BUFFER_SIZE
+)
 
-    print('[INFO] Plotting training history')
-    plotTrainingHistory('models/training_history.csv')
+model = getTransformerModel(
+    vocab_size=s.VOCAB_SIZE,
+    num_layers=s.NUM_LAYERS,
+    units=s.UNITS,
+    d_model=s.D_MODEL,
+    num_heads=s.NUM_HEADS,
+    dropout=s.DROPOUT
+)
 
+model.summary()
 
-if __name__ == '__main__':
-    main()
+# configure optimizer
+learningRate = CustomSchedule(s.D_MODEL, warmup_steps=4000)
+optimizer = Adam(learningRate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+# compile model with custom metrics
+model.compile(
+    optimizer=optimizer,
+    loss=customLossFunction,
+    metrics=[accuracy]
+)
+
+# train model with callbacks
+callbacks = getCallbacks()
+model.fit(dataset, epochs=s.EPOCHS, callbacks=callbacks)
+
+print('[INFO] Saving model weights')
+model.save_weights('models/final_model_weight')
+
+print('[INFO] Plotting training history')
+plotTrainingHistory('models/training_history.csv')

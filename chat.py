d413325881ad32959fd8da74ed752329ee8a93fe
Gautam J <gautam.jayapal@gmail.com>
2020-09-01 18:50:40 +0530

    update chat clear screen

--- chat.py
+++ chat.py
@@ -1,15 +1,20 @@
+import os
+
 from data_configs import loadTokenizer
 from utils import loadTrainedTransformerModel, predict
 
 tokenizer = loadTokenizer('models/myTokenizer.json')
 model = loadTrainedTransformerModel('models/final_model_weight')
 
+predict('Hello!', model, tokenizer)
+os.system('clear')
+
+print('Type your query and press ENTER, q to quit')
 while True:
-    userInput = input('You: ')
+    userInput = input('\nYou: ')
 
     if userInput == 'q':
         break
 
     predictedResponse = predict(userInput, model, tokenizer)
     print('AI:', predictedResponse)
-    print()

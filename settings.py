214dfb0c31262bd28799f573610a18a1b1d8b606
Gautam J <gautam.jayapal@gmail.com>
2020-09-02 15:56:11 +0530

    full_data_model

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 # Data preprocessing
-VOCAB_SIZE = 8000
-SEQUENCE_LENGTH = 30
-DATASET_SIZE = 0.22595601992028272  # percentage of total data used for training
+VOCAB_SIZE = 10000
+SEQUENCE_LENGTH = 40
+DATASET_SIZE = 1.0  # percentage of total data used for training
 BATCH_SIZE = 64
-SHUFFLE_BUFFER_SIZE = 25000
+SHUFFLE_BUFFER_SIZE = 100000
 
 # token tags
 STARTOFSENTENCE_TOKEN = '<sos>'
@@ -12,7 +12,7 @@ OUTOFVOCABULARY_TOKEN = '<oov>'
 
 # Hyperparameters
 DROPOUT = 0.1
-EPOCHS = 20
+EPOCHS = 30
 NUM_LAYERS = 2
 UNITS = 512
 D_MODEL = 256  # d_model % num_heads == 0

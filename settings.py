ab4e2f4e98e5dc096c67365c50a44b0cbfb943e9
Gautam J <gautam.jayapal@gmail.com>
2020-09-02 10:58:11 +0530

    update settings

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 # Data preprocessing
 VOCAB_SIZE = 8000
-SEQUENCE_LENGTH = 20
-DATASET_SIZE = 0.2  # percentage of total data used for training
+SEQUENCE_LENGTH = 30
+DATASET_SIZE = 0.22595601992028272  # percentage of total data used for training
+BATCH_SIZE = 64
+SHUFFLE_BUFFER_SIZE = 25000
 
 # token tags
 STARTOFSENTENCE_TOKEN = '<sos>'
@@ -9,11 +11,9 @@ ENDOFSENTENCE_TOKEN = '<eos>'
 OUTOFVOCABULARY_TOKEN = '<oov>'
 
 # Hyperparameters
-BATCH_SIZE = 32
-SHUFFLE_BUFFER_SIZE = 10000
-EPOCHS = 15
-NUM_LAYERS = 2
-NUM_HEADS = 8
-D_MODEL = 128
-UNITS = 512
 DROPOUT = 0.1
+EPOCHS = 20
+NUM_LAYERS = 2
+UNITS = 512
+D_MODEL = 256  # d_model % num_heads == 0
+NUM_HEADS = 8
